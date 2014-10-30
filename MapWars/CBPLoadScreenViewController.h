7345c1dae33407f964471a17843470536dca3e19
Christopher Paika <cpaika@edmac28.cs.umass.edu>
2014-10-29 23:02:07 -0400

    Adding in start and load screen

---
+++ MapWars/CBPLoadScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBPLoadScreenViewController.h
+//  MapWars
+//
+//  Created by Christopher Paika on 10/29/14.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBPLoadScreenViewController : UIViewController
+
+@end
---
+++ MapWars/CBPStartScreenViewController.m
@@ -0,0 +1,49 @@
+//
+//  CBPStartScreenViewController.m
+//  MapWars
+//
+//  Created by Christopher Paika on 10/29/14.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import "CBPStartScreenViewController.h"
+
+@interface CBPStartScreenViewController ()
+
+@end
+
+@implementation CBPStartScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
