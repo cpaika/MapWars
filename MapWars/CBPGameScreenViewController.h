626ad96d5be0d25314692debaa0e4e0a8e7777e3
Christopher Paika <cpaika@edmac14.cs.umass.edu>
2014-11-04 02:33:34 -0500

    Working to get ready for demo

---
+++ MapWars/CBPGameScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBPGameScreenViewController.h
+//  MapWars
+//
+//  Created by Christopher Paika on 11/3/14.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBPGameScreenViewController : UIViewController
+
+@end
---
+++ MapWars/CBPGameScreenViewController.m
@@ -0,0 +1,49 @@
+//
+//  CBPGameScreenViewController.m
+//  MapWars
+//
+//  Created by Christopher Paika on 11/3/14.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import "CBPGameScreenViewController.h"
+
+@interface CBPGameScreenViewController ()
+
+@end
+
+@implementation CBPGameScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
